b3d45cc4c975065dd424373fb4abfa660985a0cd
Václav Chlumský <chlumskyvaclav@gmail.com>
2017-04-24 08:17:06 +0200

    [external-hooks] replacement for epi-pro-logue hook

--- hook_external-hooks.py
+++ hook_external-hooks.py
@@ -5,6 +5,7 @@ import subprocess, os
 
 DIR_PROLOGUE="/var/spool/pbs/mom_priv/prologue.d/"
 DIR_EPILOGUE="/var/spool/pbs/mom_priv/epilogue.d/"
+DIR_END="/var/spool/pbs/mom_priv/end.d/"
 
 if not DIR_PROLOGUE.endswith("/"):
     DIR_PROLOGUE += "/"
@@ -12,12 +13,15 @@ if not DIR_PROLOGUE.endswith("/"):
 if not DIR_EPILOGUE.endswith("/"):
     DIR_EPILOGUE += "/"
 
+if not DIR_END.endswith("/"):
+    DIR_END += "/"
+
 def is_executable(fpath):
     return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
 def run_file(fpath):
     try:
-        pbs.logmsg(pbs.EVENT_DEBUG, "Executing prologue/epilogue started: %s" % fpath)
+        pbs.logmsg(pbs.EVENT_DEBUG, "external hook started: %s" % fpath)
         command = fpath
         #new_env = os.environ.copy()
         new_env = j.Variable_List
@@ -27,17 +31,17 @@ def run_file(fpath):
         new_env['HOSTNAME'] = socket.gethostname()
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=new_env)
         (out, err) = proc.communicate()
-        pbs.logmsg(pbs.EVENT_DEBUG, "%s stdout: '%s' stderr: '%s'" % (fpath, out.replace("\n",","), err.replace("\n",",")))
-        pbs.logmsg(pbs.EVENT_DEBUG, "Executing prologue/epilogue ended with exitcode: %d" % proc.returncode)
+        pbs.logmsg(pbs.EVENT_DEBUG, "external hook %s stdout: '%s' stderr: '%s'" % (fpath, out.replace("\n",","), err.replace("\n",",")))
+        pbs.logmsg(pbs.EVENT_DEBUG, "external hook ended with exitcode: %d" % proc.returncode)
     except Exception as error:
-        pbs.logmsg(pbs.EVENT_DEBUG, "Executing prologue/epilogue %s failed: %s" % (fpath, str(error)))
+        pbs.logmsg(pbs.EVENT_DEBUG, "external hook %s failed: %s" % (fpath, str(error)))
 
 def run_dir(path):
     if not os.path.isdir(path):
-        pbs.logmsg(pbs.EVENT_DEBUG, "No prologue/epilogue dir: %s" % path)
+        pbs.logmsg(pbs.EVENT_DEBUG, "no external hook dir: %s" % path)
         e.accept()
 
-    for file in os.listdir(path):
+    for file in sorted(os.listdir(path)):
         fpath = path + file
 
         if is_executable(fpath):
@@ -53,8 +57,12 @@ try:
     if e.type == pbs.EXECJOB_EPILOGUE:
         j = e.job
         run_dir(DIR_EPILOGUE)
+
+    if e.type == pbs.EXECJOB_END:
+        j = e.job
+        run_dir(DIR_END)
         
 except SystemExit:
     pass
 except:
-    e.reject("prologue/epilogue hook failed")
+    e.reject("external-hooks hook failed")
