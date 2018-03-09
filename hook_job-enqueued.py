fd7112600bfd200e3d7c57bbe96b2c4372496b6b
Václav Chlumský <chlumskyvaclav@gmail.com>
2018-03-09 11:34:19 +0100

    [job-enqueued] update mpiprocs and ompthreads on job submission

--- hook_job-enqueued.py
+++ hook_job-enqueued.py
@@ -9,20 +9,50 @@ try:
             pbs.logmsg(pbs.EVENT_DEBUG,"Resource_List: " + str(j.Resource_List.keys()))
             e.reject("Old syntax rejected. Please use 'select' syntax.")
 
-        #  mpiprocs se ma rovnat ncpus
+        #  mpiprocs = ncpus/ompthreads
+        #  ompthreads = ncpus/mpiprocs
         if "select" in j.Resource_List.keys():
             newselect = []
             for i in str(j.Resource_List["select"]).split("+"):
+                ncpus = 0
+                ompthreads = 0
+                mpiprocs = 0
+
                 m = re.search('.*ncpus=([0-9]+).*', i)
                 if m:
-                    ncpus = m.group(1)
+                    ncpus = int(m.group(1))
                 else:
                     newselect.append(i)
                     continue
 
                 m = re.search('.*mpiprocs=([0-9]+).*', i)
-                if not m:
-                    i += ":mpiprocs=" + str(ncpus)
+                if m:
+                    mpiprocs = int(m.group(1))
+
+                m = re.search('.*ompthreads=([0-9]+).*', i)
+                if m:
+                    ompthreads = int(m.group(1))
+
+                if not mpiprocs and not ompthreads:
+                    i += ":mpiprocs=%d:ompthreads=1" % ncpus
+
+                elif mpiprocs and not ompthreads:
+                    try:
+                        ompthreads = ncpus/mpiprocs
+                        if not ompthreads:
+                            ompthreads = 1
+                    except:
+                        ompthreads = 1
+                    i += ":ompthreads=%s" % ompthreads
+
+                elif not mpiprocs and ompthreads:
+                    try:
+                        mpiprocs = ncpus/ompthreads
+                        if not mpiprocs:
+                            mpiprocs = 1
+                    except:
+                        mpiprocs = 1
+                    i += ":mpiprocs=%d" % mpiprocs
 
                 newselect.append(i)
 
@@ -37,5 +67,5 @@ try:
 
 except SystemExit:
     pass
-except:
-    e.reject("job-enqueued hook failed")
+except Exception as err:
+    e.reject("job-enqueued hook failed with error: %s" % str(err))
